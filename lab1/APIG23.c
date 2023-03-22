ea3f40be744db23d3a2ef5eb3b4950bc7db1ba26
irmrz <ignaciotramirez02@gmail.com>
2023-03-22 20:10:37 -0300

    agregando archivo para implementacion

---
+++ lab1/APIG23.c
@@ -0,0 +1,2 @@
+#include "APIG23.h"
+#include "EstructuraGrafo23.h"
---
+++ lab1/EstructuraGrafo23.h
@@ -0,0 +1,10 @@
+#ifndef EstructuraGrafos3_H
+#define EstructuraGrafos3_H
+
+#include "APIG23.h"
+#include <stdint.h>
+
+/* Definición de u32*/
+typedef unsigned int u32;
+
+#endif
